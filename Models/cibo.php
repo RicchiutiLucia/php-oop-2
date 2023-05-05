b855c3b8eec7b2d1c785e6b22ea2d43092b63abc
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-05 14:37:03 +0200

    aggiunto classe cibo

---
+++ Models/cibo.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/prodotto.php';
+
+class Cibo extends Prodotto{
+    public $pesonetto;
+    public $ingredienti;
+
+
+    public function __construct(
+        String $immagine,
+        String $nome,
+        Float $prezzo,
+        Categorie $category,
+        Float $pesonetto,
+        String $ingredienti
+
+        ) {
+        $this->pesonetto = $pesonetto;
+        $this->ingredienti = $ingredienti;
+        parent ::__construct($immagine, $nome, $prezzo, $category);
+              
+}
+}
